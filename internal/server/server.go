edd97129076201f19c7649d85da42341eb8d5de9
Milan Brkic <milan.brkic1998@gmail.com>
2023-11-08 02:46:19 +0100

    switched to using gin and using json unmarshall midleware

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,21 +4,54 @@ import (
 	"fmt"
 	"go-chat/internal/config"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+func JSONUnmarshalMiddleware(c *gin.Context) {
+	var requestData map[string]interface{}
+	if err := c.ShouldBindJSON(&requestData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+		c.Abort()
+		return
+	}
+
+	c.Set("jsonData", requestData)
+	c.Next()
+}
 
 func Listen() {
 	port := config.SERVER_PORT
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//     w.Write([]byte("Hello, World!"))
-	// })
 
-	server := &http.Server{Addr: ":" + port}
+	r := gin.Default()
+
+	gin.SetMode(gin.ReleaseMode)
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(JSONUnmarshalMiddleware)
+
+	r.POST("/process-json", func(c *gin.Context) {
+		jsonData, ok := c.MustGet("jsonData").(map[string]interface{})
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "JSON data not found in request"})
+			return
+		}
+
+		field1, _ := jsonData["milan"].(string)
+
+		response := gin.H{
+			"Field1": field1,
+		}
+
+		c.JSON(http.StatusOK, response)
+	})
 
 	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			panic(err)
+		if err := r.Run(":" + port); err != nil {
+			fmt.Printf("Server error: %v\n", err)
 		}
 	}()
 
-	fmt.Printf("Server listening on port %s...\n", port)
+	fmt.Printf("Server listening on port %s\n", port)
 }
